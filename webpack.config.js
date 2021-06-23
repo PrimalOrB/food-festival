1a1f0330853653c7f82ce0cd955cf0592c5598eb
Andrew Ogilvie <primalorb@gmail.com>
2021-06-22 21:45:18 -0400

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path =  require( 'path' );
+const webpack = require( 'webpack' );
+
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: path.resolve( __dirname, '/dist' ),
+        filename: 'main.bundle.js'
+    },
+    plugins:[
+        new webpack.ProvidePlugin ({
+          $: "jquery",
+          jQuery: "jquery"
+        } ),
+    ],
+    devtool: "eval-cheap-source-map",
+    mode: 'development'
+}
